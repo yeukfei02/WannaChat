2f13998ffc686907e1962ef0dc374b7194e49705
Donald Wu <yeukfei02@gmail.com>
2019-10-17 22:47:56 +0800

    add lib

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	cors "github.com/gin-contrib/cors"
+	"github.com/gin-contrib/gzip"
 	"github.com/gin-gonic/gin"
 
 	"WannaChat/routes/groupRoutes"
@@ -12,6 +13,7 @@ import (
 func main() {
 	r := gin.Default()
 	r.Use(cors.Default())
+	r.Use(gzip.Gzip(gzip.DefaultCompression))
 
 	userRoutes.Routes(r)
 	groupRoutes.Routes(r)
